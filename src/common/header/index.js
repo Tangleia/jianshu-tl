65328fcc41d4442c82f570a434c0d0026a2a58b9
快食空 <kuaishikong@163.com>
2021-01-12 20:42:57 +0800

    修改图标

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,12 +1,32 @@
-import React , { Component } from 'react';
+import React, {Component} from 'react';
+//import { CSSTransition } from 'react-transition-group';
 import {
-    HeaderWrapper
+    Addition,
+    HeaderWrapper, Logo, Nav, NavItem, NavSearch, Button
 } from "./style";
 
 class Header extends Component {
     render() {
         return (
-            <HeaderWrapper>header</HeaderWrapper>
+            <HeaderWrapper>
+                <Logo/>
+                <Nav>
+                    <NavItem className="left active">首页</NavItem>
+                    <NavItem className="left">下载App</NavItem>
+                    <NavItem className="right">登录</NavItem>
+                    <NavItem className="right">
+                        <img className="img-icon" alt="" src="img/a.png"/>
+                    </NavItem>
+                    <NavSearch/>
+                </Nav>
+                <Addition>
+                    <Button className="wri">
+                        <img className="img-icon" alt="" src="img/bi.png"/>
+                        写文章
+                    </Button>
+                    <Button className="reg">注册</Button>
+                </Addition>
+            </HeaderWrapper>
         )
     }
 }
